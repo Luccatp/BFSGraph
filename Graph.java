2a7f4349f31f83fb12d4ccee2febb1e79f91a216
Lucca Paradeda <luccatisserparadeda@gmail.com>
2023-06-08 21:13:55 -0300

    fix: gasoline

--- Graph.java
+++ Graph.java
@@ -23,7 +23,7 @@ public class Graph {
     public Graph(char[][] map) {
         this.map = map;
         this.V = map.length * map[0].length;
-        
+
         for (int row = 0; row < map.length; row++) {
             for (int col = 0; col < map[0].length; col++) {
                 char digit = map[row][col];
@@ -33,7 +33,7 @@ public class Graph {
                     ports.add(port);
                     point.setIsPort();
                 }
-                
+
                 points.put(Integer.toString(col) + Integer.toString(row), point);
             }
         }
@@ -44,30 +44,29 @@ public class Graph {
         LinkedList<Point> fila = new LinkedList<>();
         Map<String, Point> pais = new HashMap<>();
         Set<String> visitados = new HashSet<>();
-        //System.out.println(destino.getValue());
+        // System.out.println(destino.getValue());
         fila.offer(this.points.get(inicio.getKey()));
         visitados.add(inicio.getKey());
 
         while (!fila.isEmpty()) {
             Point actualPosition = fila.poll();
-            gasoline++;
-            if(Character.isDigit(actualPosition.getValue())) {
+            if (Character.isDigit(actualPosition.getValue())) {
                 if (Character.digit(actualPosition.getValue(), 10) == destino.getValue()) {
-                    //System.out.println("Encontrei");
-                    //System.out.println("aaa");
+                    // System.out.println("Encontrei");
+                    // System.out.println("aaa");
                     return reconstruirCaminho(this.points.get(inicio.getKey()), actualPosition, pais);
                 }
             }
-            
-            //System.out.print(actualPosition.getValue());
+
+            // System.out.print(actualPosition.getValue());
             List<Point> neighbors = getNeighborhood(actualPosition);
-            
+
             for (Point neighbor : neighbors) {
                 if (neighbor != null && !visitados.contains(neighbor.getKey())) {
                     fila.offer(neighbor);
                     visitados.add(neighbor.getKey());
                     pais.put(neighbor.getKey(), actualPosition);
-                } 
+                }
             }
         }
         return null;
@@ -76,10 +75,10 @@ public class Graph {
     public List<Point> getNeighborhood(Point actualPosition) {
         List<Point> neightbors = new ArrayList<>();
         int[][] direcoes = {
-            {-1, 0},
-            {1, 0},
-            {0, -1},
-            {0, 1}
+                { -1, 0 },
+                { 1, 0 },
+                { 0, -1 },
+                { 0, 1 }
         };
 
         int lines = this.map.length;
@@ -88,7 +87,7 @@ public class Graph {
         for (int[] direcao : direcoes) {
             int newCol = actualPosition.getCol() + direcao[0];
             int newRow = actualPosition.getRow() + direcao[1];
-            
+
             if (newRow >= 0 && newRow < lines && newCol >= 0 && newCol < columns && this.map[newRow][newCol] != '*') {
                 neightbors.add(this.points.get(Integer.toString(newCol) + Integer.toString(newRow)));
             }
@@ -97,11 +96,12 @@ public class Graph {
         return neightbors;
     }
 
-    public static List<int[]> reconstruirCaminho(Point inicio, Point destino, Map<String, Point> pais) {
+    public List<int[]> reconstruirCaminho(Point inicio, Point destino, Map<String, Point> pais) {
         List<int[]> caminho = new ArrayList<>();
         Point actualPosition = destino;
 
-        while (!actualPosition.getKey().equals(inicio.getKey()) ) {
+        while (!actualPosition.getKey().equals(inicio.getKey())) {
+            this.gasoline++;
             int[] position = new int[2];
             position[0] = actualPosition.getRow();
             position[1] = actualPosition.getCol();
@@ -118,12 +118,12 @@ public class Graph {
     }
 
     public void getResults() {
-        //System.out.println(ports.get(7).getValue());
+        // System.out.println(ports.get(7).getValue());
 
-        for (int i = 0; i < ports.size()-1; i++) {
+        for (int i = 0; i < ports.size() - 1; i++) {
             System.out.println(ports.get(i).getValue());
             List<int[]> pathTo = bfs(ports.get(i), ports.get(i + 1));
-            if(pathTo == null) {
+            if (pathTo == null) {
                 System.out.println("Sem caminho para o port " + ports.get(i + 1).getValue());
                 continue;
             }
@@ -133,6 +133,6 @@ public class Graph {
             System.out.println();
             pathTo.clear();
         }
-        System.out.println("Gasolina gasta para ir e voltar entre os portos: " + this.gasoline*2);
+        System.out.println("Gasolina gasta para ir e voltar entre os portos: " + this.gasoline * 2);
     }
 }
